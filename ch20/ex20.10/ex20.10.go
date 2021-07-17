cab1168bdb8bb91f4b3e01175902d3654ae35337
thisisnew <thisisnew@naver.com>
2021-07-18 01:00:36 +0900

    챕터20 공부

---
+++ ch20/ex20.10/ex20.10.go
@@ -0,0 +1,26 @@
+package main
+
+type Reader interface {
+	Read()
+}
+
+type Closer interface {
+	Close()
+}
+
+type File struct {
+}
+
+func (f *File) Read() {
+
+}
+
+func ReadFile(reader Reader) {
+	c := reader.(Closer)
+	c.Close()
+}
+
+func main() {
+	file := &File{}
+	ReadFile(file)
+}
