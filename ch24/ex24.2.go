c83fecd428ced85029c0a642d20f348a583c8b64
thisisnew <thisisnew@naver.com>
2021-07-19 23:07:26 +0900

    챕터24 공부

---
+++ ch24/ex24.2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func SumAtoB(a, b int) {
+	sum := 0
+	for i := a; i <= b; i++ {
+		sum += i
+	}
+
+	fmt.Printf("%d부터 %d까지 합계는 %d입니다.\n", a, b, sum)
+	wg.Done()
+}
+
+func main() {
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go SumAtoB(1, 10000000)
+	}
+
+	wg.Wait()
+	fmt.Println("모든 계산이 완료되었습니다")
+}
