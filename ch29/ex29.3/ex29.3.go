d421f872d636032218b8d1ce66469656c3f9ec44
thisisnew <thisisnew@naver.com>
2021-07-20 23:50:43 +0900

    챕터29 공부

---
+++ ch29/ex29.3/ex29.3.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+
+	mux.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello Bar")
+	})
+
+	http.ListenAndServe(":3000", mux)
+}
