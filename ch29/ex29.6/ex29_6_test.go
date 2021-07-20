d56d085a1e2051323f176dfd0385043cb22e3c5d
thisisnew <thisisnew@naver.com>
2021-07-21 00:23:25 +0900

    챕터29 공부

---
+++ ch29/ex29.6/ex29_6_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"src/github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestJsonHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/student", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	student := new(Student)
+	err := json.NewDecoder(res.Body).Decode(student)
+	assert.Nil(err)
+	assert.Equal("aaa", student.Name)
+	assert.Equal(16, student.Age)
+	assert.Equal(87, student.Score)
+}
