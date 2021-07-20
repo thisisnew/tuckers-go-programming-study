f334e7dd4fc44f1070495e6f6f1ee259e2f1b6c4
thisisnew <thisisnew@naver.com>
2021-07-20 23:03:07 +0900

    챕터28 공부

--- ch28/ex28.1/ex28.1_test.go
+++ ch28/ex28.1/ex28.1_test.go
@@ -1,19 +1,16 @@
 package main
 
-import "testing"
+import (
+	"src/github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestSquare1(t *testing.T) {
-	rst := square(9)
-
-	if rst != 81 {
-		t.Errorf("square(9) should be 81 but square(9) return %d", rst)
-	}
+	assert := assert.New(t)
+	assert.Equal(81, square(9), "square(9) should be 81")
 }
 
 func TestSquare2(t *testing.T) {
-	rst := square(3)
-
-	if rst != 9 {
-		t.Errorf("square(3) should be 9 but square(3) return %d", rst)
-	}
+	assert := assert.New(t)
+	assert.Equal(9, square(3), "square(3) should be 9")
 }
