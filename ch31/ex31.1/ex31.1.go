68ceeb05dd3d364524fc585c2a0097ba57b6e993
thisisnew <thisisnew@naver.com>
2021-07-21 22:24:51 +0900

    챕터31 공부

---
+++ ch31/ex31.1/ex31.1.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sort"
+	"src/github.com/gorilla/mux"
+	"src/github.com/unrolled/render"
+	"src/github.com/urfave/negroni"
+	"strconv"
+)
+
+var rd *render.Render
+
+type Todo struct {
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed,omitempty"`
+}
+
+var todoMap map[int]Todo
+var lastID int = 0
+
+func MakeWebHandler() http.Handler {
+	todoMap = make(map[int]Todo)
+	mux := mux.NewRouter()
+	mux.Handle("/", http.FileServer(http.Dir("public")))
+	mux.HandleFunc("/todos", GetTodoListHandler).Methods(http.MethodGet)
+	mux.HandleFunc("/todos", PostTodoHandler).Methods(http.MethodPost)
+	mux.HandleFunc("/todos/{id:[0-9]+}", RemoveTodoHandler).Methods(http.MethodDelete)
+	mux.HandleFunc("/todos/{id:[0-9]+}", UpdateTodoHandler).Methods(http.MethodPut)
+	return mux
+}
+
+type Todos []Todo
+
+func (t Todos) Len() int {
+	return len(t)
+}
+
+func (t Todos) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t Todos) Less(i, j int) bool {
+	return t[i].ID < t[j].ID
+}
+
+func GetTodoListHandler(w http.ResponseWriter, r *http.Request) {
+	list := make(Todos, 0)
+	for _, todo := range todoMap {
+		list = append(list, todo)
+	}
+	sort.Sort(list)
+	rd.JSON(w, http.StatusOK, list)
+}
+
+func PostTodoHandler(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	lastID++
+	todo.ID = lastID
+	todoMap[lastID] = todo
+	rd.JSON(w, http.StatusCreated, todo)
+}
+
+type Success struct {
+	Success bool `json:"success"`
+}
+
+func RemoveTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if _, ok := todoMap[id]; ok {
+		delete(todoMap, id)
+		rd.JSON(w, http.StatusOK, Success{Success: true})
+	} else {
+		rd.JSON(w, http.StatusNotFound, Success{Success: false})
+	}
+}
+
+func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
+	var newTodo Todo
+	err := json.NewDecoder(r.Body).Decode(&newTodo)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if todo, ok := todoMap[id]; ok {
+		todo.Name = newTodo.Name
+		todo.Completed = newTodo.Completed
+		rd.JSON(w, http.StatusOK, Success{Success: true})
+	} else {
+		rd.JSON(w, http.StatusBadRequest, Success{Success: false})
+	}
+}
+
+func main() {
+	rd = render.New()
+	m := MakeWebHandler()
+	n := negroni.Classic()
+	n.UseHandler(m)
+
+	log.Println("Started App")
+	err := http.ListenAndServe(":3000", n)
+	if err != nil {
+		panic(err)
+	}
+}
