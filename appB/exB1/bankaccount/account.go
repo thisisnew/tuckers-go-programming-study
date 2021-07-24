1ce06dd74260feda4eee21c8ce9ec44e5b521fb5
thisisnew <thisisnew@naver.com>
2021-07-24 11:00:00 +0900

    생각하는 프로그래밍 공부

---
+++ appB/exB1/bankaccount/account.go
@@ -0,0 +1,27 @@
+package bankaccount
+
+type Account interface {
+	Withdraw(money int) int
+	Deposit(money int)
+	Balance() int
+}
+
+func NewAccount() Account {
+	return &innerAccount{balance: 1000}
+}
+
+type innerAccount struct {
+	balance int
+}
+
+func (a *innerAccount) Withdraw(money int) int {
+	a.balance -= money
+	return a.balance
+}
+func (a *innerAccount) Deposit(money int) {
+	a.balance += money
+}
+
+func (a *innerAccount) Balance() int {
+	return a.balance
+}
--- appB/exB1/exB1.go
+++ appB/exB1/exB1.go
@@ -1,1 +1,12 @@
-package exB1
+package main
+
+import (
+	"fmt"
+	"tuckers-go-programming-study/appB/exB1/bankaccount"
+)
+
+func main() {
+	account := bankaccount.NewAccount()
+	account.Deposit(1000)
+	fmt.Println(account.Balance())
+}
