b80f8f2f60edb7fc3566a2f1a7607d1ff2fc3d27
thisisnew <thisisnew@naver.com>
2021-07-25 21:24:22 +0900

    생각하는 프로그래밍 공부

--- appB/exB2/exB2.go
+++ appB/exB2/exB2.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
+type Student struct {
+	name string
+	age  int
+}
+
 func main() {
+	var s Student
+	var p *Student
+
+	p = &s
+	s.name = "bbb"
+
+	fmt.Println("s:", s)
+	fmt.Println("p:", p)
+
 	var array [5]int = [5]int{1, 2, 3, 4, 5}
 	var b [5]int
 	b = array
